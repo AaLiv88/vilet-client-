6a27c6f9d3ae4a7de54d1b023ed1fca1dd401574
sasha <leonov.sas2019@yadnex.ru>
2022-11-04 07:00:29 +0300

    сделал добавление работ

--- src/pages/Admin/AdminForm/AdminForm.tsx
+++ src/pages/Admin/AdminForm/AdminForm.tsx
@@ -1,53 +1,37 @@
 import React, { FC, useState } from 'react';
-import cl from "../AdminPage.module.scss";
-// import { Button, Form, Input } from "antd";
 import { useAppDispatch } from "../../../hooks/redux";
 import { login } from "../../../redux/actionCreators/UserActionCreators";
+import cl from "./AdminForm.module.scss";
 
 const AdminForm: FC = () => {
     const dispatch = useAppDispatch();
-    const [name, setName] = useState("admin");
-    const [password, setPassword] = useState("admin");
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
 
-    const submit = () => {
-        dispatch(login(name, password))
+    const onAdminLogin = () => {
+        dispatch(login(name, password));
     }
 
     return (
-        <div>
-            {/*<div className={cl.root}>*/}
-            {/*    <Form*/}
-            {/*        name="basic"*/}
-            {/*        labelCol={{ span: 8 }}*/}
-            {/*        wrapperCol={{ span: 16 }}*/}
-            {/*        initialValues={{ remember: true }}*/}
-            {/*        onFinish={submit}*/}
-            {/*        // onFinishFailed={onFinishFailed}*/}
-            {/*        autoComplete="off"*/}
-            {/*    >*/}
-            {/*        <Form.Item*/}
-            {/*            label="Username"*/}
-            {/*            name="username"*/}
-            {/*            rules={[{ required: true, message: 'Please input your username!' }]}*/}
-            {/*        >*/}
-            {/*            <Input/>*/}
-            {/*        </Form.Item>*/}
-
-            {/*        <Form.Item*/}
-            {/*            label="Password"*/}
-            {/*            name="password"*/}
-            {/*            rules={[{ required: true, message: 'Please input your password!' }]}*/}
-            {/*        >*/}
-            {/*            <Input.Password/>*/}
-            {/*        </Form.Item>*/}
-
-            {/*        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>*/}
-            {/*            <Button type="primary" htmlType="submit">*/}
-            {/*                Submit*/}
-            {/*            </Button>*/}
-            {/*        </Form.Item>*/}
-            {/*    </Form>*/}
-            {/*</div>*/}
+        <div className={cl.root}>
+            <input
+                className={cl.formItem}
+                type="text"
+                value={name}
+                onChange={e => setName(e.target.value)}
+            />
+            <input
+                className={cl.formItem}
+                type="text"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+            />
+            <button
+                className={cl.formItem}
+                onClick={onAdminLogin}
+            >
+                Войти
+            </button>
         </div>
     );
 };
